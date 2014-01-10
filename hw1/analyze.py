8ecb1dd9d671eae3766e9280498961bdab5e52ed
Jasen Hall <jasen@cs.ucsb.edu>
2014-01-10 08:50:24 -0800

    Added averaging of multiple runs per matrix size to get average times.

--- hw1/analyze.py
+++ hw1/analyze.py
@@ -4,22 +4,41 @@ import commands
 import re
 
 MAXTIME = 600.0
+ITERATIONS = 4
+DEBUG = False
 
 f = open("hw1data.csv", "w")
 
 for algo in range(1, 8):
-        print('Algorithm ' + str(algo) +':')
+        if DEBUG:
+          print('Algorithm ' + str(algo) +':')
+
         matdim = 2
         multtime = 0
+        tottime = 0
+
         while multtime < MAXTIME:
-            print('\t' + str(matdim) + 'x' + str(matdim) + 'matrices')
+
+          itercount = 0
+          while itercount < ITERATIONS:
+
+            if DEBUG:
+              print('\t' + str(matdim) + 'x' + str(matdim) + 'matrices, run ' + str(itercount + 1))
+
             cmd = "./matrix_multiply -n" + str(matdim) + " -a" + str(algo)
             o = commands.getoutput(cmd)
             multtime = float(re.search('Time \= (.*) sec, .*', o).group(1))
-            f.write(str(multtime) + " ")
-            print('\t\trun time ' + str(multtime))
-            matdim = matdim << 1
+            tottime = tottime + multtime
+
+            if DEBUG:
+              print('\t\trun time ' + str(multtime))
+
+            itercount = itercount + 1
+
+          f.write(str(tottime/float(ITERATIONS)) + ", ")
+          matdim = matdim << 1
 
         f.write('\n')
 
-print('\nAnalysis complete.')
+if DEBUG:
+  print('\nAnalysis complete.')
